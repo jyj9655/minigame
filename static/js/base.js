3f5c06e580869edc2bc3edf987f7411400b6fd15
jyj9655 <enscl5575@gmail.com>
2024-01-17 12:23:25 +0900

    base nav css 변경

---
+++ static/js/base.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('active'); // 메뉴의 'active' 클래스 토글
+    });
+});
